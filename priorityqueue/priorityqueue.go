acb3cd276dc94640300a310c8efce7e7f0fe5318
MapleJupiter <MapleJupiter@gitlab.com>
2025-03-04 13:49:31 +0800

    Properly move the priority queue code to its own package, to be imported by main.dijkstra

--- priorityqueue/priorityqueue.go
+++ priorityqueue/priorityqueue.go
@@ -1,8 +1,8 @@
 package priorityqueue
 
 type priorityQueueItem struct {
-	value    int
-	priority int
+	Value    uint
+	Priority uint
 }
 
 type PriorityQueue struct {
@@ -27,7 +27,7 @@ func (pq *PriorityQueue) sinkDown() {
 
 		if leftChildIdx < length {
 			leftChild = pq.items[leftChildIdx]
-			if leftChild.priority < element.priority {
+			if leftChild.Priority < element.Priority {
 				swapIdx = leftChildIdx
 			}
 		}
@@ -35,11 +35,11 @@ func (pq *PriorityQueue) sinkDown() {
 		if rightChildIdx < length {
 			rightChild = pq.items[rightChildIdx]
 			if swapIdx < 0 {
-				if rightChild.priority < element.priority {
+				if rightChild.Priority < element.Priority {
 					swapIdx = rightChildIdx
 				}
 			} else {
-				if rightChild.priority < leftChild.priority {
+				if rightChild.Priority < leftChild.Priority {
 					swapIdx = rightChildIdx
 				}
 			}
@@ -63,7 +63,7 @@ func (pq *PriorityQueue) bubbleUp() {
 		parentIdx := (idx - 1) / 2
 		parentItem := pq.items[parentIdx]
 
-		if elem.priority >= parentItem.priority {
+		if elem.Priority >= parentItem.Priority {
 			break
 		}
 
@@ -73,24 +73,15 @@ func (pq *PriorityQueue) bubbleUp() {
 	}
 }
 
-func (pq *PriorityQueue) Enqueue(val, prior int) {
+func (pq *PriorityQueue) Enqueue(val uint, prior uint) {
 	pq.items = append(pq.items, priorityQueueItem{
-		value:    val,
-		priority: prior,
+		Value:    val,
+		Priority: prior,
 	})
 	pq.bubbleUp()
 }
 
 func (pq *PriorityQueue) Dequeue() priorityQueueItem {
-	/*
-	   min_item = self.items[0]
-	   end_item = self.items.pop()
-	   if len(self.items) > 0:
-	       self.items[0] = end_item
-	       self.sink_down()
-	   return min_item
-	*/
-
 	itemsMaxIdx := len(pq.items) - 1
 	minItem := pq.items[0]
 	endItem := pq.items[itemsMaxIdx]
@@ -103,74 +94,14 @@ func (pq *PriorityQueue) Dequeue() priorityQueueItem {
 	return minItem
 }
 
+func (pq *PriorityQueue) OverwriteMinItem(val uint, prior uint) {
+	pq.items[0] = priorityQueueItem{
+		Value:    val,
+		Priority: prior,
+	}
+	pq.sinkDown()
+}
+
 func (pq PriorityQueue) Peek() priorityQueueItem {
 	return pq.items[0]
 }
-
-/*
-   def __init__(self):
-       self.items = []
-
-   def sink_down(self):
-       idx = 0
-       length = len(self.items)
-       element = self.items[0]
-       while True:
-           left_child_idx = (2 * idx) + 1
-           right_child_idx = left_child_idx + 1
-           left_child = None
-           right_child = None
-           swap = None
-
-           if left_child_idx < length:
-               left_child = self.items[left_child_idx]
-               if left_child[1] < element[1]:
-                   swap = left_child_idx
-
-           if right_child_idx < length:
-               right_child = self.items[right_child_idx]
-               if (swap is None and right_child[1] < element[1]) or (swap is not None and right_child[1] < left_child[1]):
-                  swap = right_child_idx
-
-           if swap is None:
-               break
-
-           self.items[idx] = self.items[swap]
-           self.items[swap] = element
-           idx = swap
-
-   def bubble_up(self):
-       idx = len(self.items) - 1
-       element = self.items[idx]
-
-       while idx > 0:
-           parent_idx = (idx - 1) // 2
-           parent_item = self.items[parent_idx]
-           if element[1] >= parent_item[1]:
-               break
-
-           self.items[parent_idx] = element
-           self.items[idx] = parent_item
-           idx = parent_idx
-
-   def enqueue(self, value, priority):
-       self.items.append(
-           (value, priority)
-       )
-       self.bubble_up()
-
-   def dequeue(self):
-       min_item = self.items[0]
-       end_item = self.items.pop()
-       if len(self.items) > 0:
-           self.items[0] = end_item
-           self.sink_down()
-       return min_item
-
-   def overwrite_min_item(self, value, priority):
-       self.items[0] = (value, priority)
-       self.sink_down()
-
-   def peek(self):
-       return self.items[0]
-*/
